c37ba4a6d0cf671e4554a3ecb9c0b154536d012a
Abdellahz0 <abdellahzeghmar@gmail.com>
2025-02-07 01:17:14 +0100

    userschema

--- src/db/init.js
+++ src/db/init.js
@@ -1,1 +1,12 @@
 //  connection to the mongodb database, and exporting the initDatabase function 
+import mongoose from 'mongoose'
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect()
+        console.log("MongoDB connected: ",conn.connection.host)
+    } catch (error) {
+        console.log("MongoDB connection error ",error)
+        process.exit(1)
+    }
+}
---
+++ src/db/models/userSchema.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    },
+    email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    },
+    password: {
+    type: String,
+    required: true,
+    minlength:8,
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    },
+    skills: {
+        type: [String],
+        default: [],
+    },
+    createdProjects: [{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'Project',     
+    }],
+    joinedProjects: [{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'Project',
+    }],
+    
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
+
+
+
